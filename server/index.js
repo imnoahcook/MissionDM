4c401afdaeeab71f3c4f01458d87785c211504f2
Cooklord <imnoahcook@gmail.com>
2019-12-24 19:44:04 -0500

    clean up index.js by removing comments

--- server/index.js
+++ server/index.js
@@ -1,20 +1,4 @@
 import dotenv from 'dotenv/config';
 
-// import './db/connection';
-// import './sequelize/config';
 import './webserver';
-// import '#root/db/connection';
 import '#root/db/models';
-
-// import Game from '#root/db/models/Game';
-
-// const game = new Game({
-//   name: "Cookie's new game",
-//   players: [],
-//   admins: [],
-// });
-
-// game.save(function(err, manga) {
-//   if (err) return console.log(err);
-//   console.log('saved');
-// });
