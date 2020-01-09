8059656993a1b9b2768564422061b5af494e1a15
Cooklord <imnoahcook@gmail.com>
2020-01-09 10:07:28 -0500

    add createUserSession resolver to graphql mutations

---
+++ server/graphql/resolvers/Mutation/createUserSession.js
@@ -0,0 +1,31 @@
+import { User, UserSession } from '#root/db/models';
+import generateUUID from '#root/helpers/generateUUID';
+import { addHours } from 'date-fns';
+
+const USER_SESSION_EXPIRY_HOURS = 1;
+
+const createUserSessionResolver = async (obj, { fbid }, context) => {
+  // console.log(context);
+  const user = await User.findOne({
+    attributes: {},
+    where: { fbid: fbid },
+  });
+
+  if (!user) return new Error('Invalid fbid!');
+
+  const expiresAt = addHours(new Date(), USER_SESSION_EXPIRY_HOURS);
+
+  const sessionToken = generateUUID();
+
+  const userSession = await UserSession.create({
+    expiresAt,
+    id: sessionToken,
+    userId: user.id,
+  });
+
+  context.res.cookie('userSessionId', userSession.id, { httpOnly: true });
+
+  return userSession;
+};
+
+module.exports = createUserSessionResolver;
--- server/graphql/resolvers/Mutation/index.js
+++ server/graphql/resolvers/Mutation/index.js
@@ -2,3 +2,4 @@ export { default as createGame } from './createGame';
 export { default as createTeam } from './createTeam.js';
 export { default as createPlayer } from './createPlayer';
 export { default as createUser } from './createUser';
+export { default as createUserSession } from './createUserSession';
