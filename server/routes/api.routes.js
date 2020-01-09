572f18f307df441d846508e0d1db5cb40476214b
Cooklord <imnoahcook@gmail.com>
2020-01-08 21:52:05 -0500

    use fbid as key for user instead of email

--- server/routes/api.routes.js
+++ server/routes/api.routes.js
@@ -1,18 +1,8 @@
 import express from 'express';
-import passport from 'passport';
+import setupUserRoutes from './users.routes';
 
 const router = express.Router();
 
-router.get('/test', async (req, res, next) => {
-  try {
-    console.log('test worked');
-    // passport.authenticate('facebook');
-
-    return res.end();
-  } catch (e) {
-    console.log('test did not work');
-    return next(e);
-  }
-});
+setupUserRoutes(router);
 
 module.exports = router;
