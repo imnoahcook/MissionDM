a031e7ef73c74e15b91196253d80cdd174891e92
ctcook97 <ctcook97@gmail.com>
2020-01-11 22:29:53 -0500

    Change game query

--- server/graphql/resolvers/Query/games.js
+++ server/graphql/resolvers/Query/games.js
@@ -1,8 +1,18 @@
-import { Game } from '#root/db/models';
+import { Game, Player } from '#root/db/models';
 
-const gamesResolver = (obj, _, context) => {
+const gamesResolver = async (obj, _, context) => {
   const { userId } = context.res.locals.userSession.dataValues;
-  return Game.findAll();
+  console.log(userId);
+  const players = await Player.findAll({
+    where: { id: userId },
+  });
+
+  if (!players) return 'invalid user ID';
+
+  const gameIds = players.map(player => player.gameId);
+  return Game.findAll({
+    where: { id: gameIds },
+  });
 };
 
 export default gamesResolver;
