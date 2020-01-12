2447c5e9dadde75be11c4ac90cf352e67dbad0f9
ctcook97 <ctcook97@gmail.com>
2020-01-12 16:40:58 -0500

    createGame works

--- server/graphql/resolvers/Mutation/createGame.js
+++ server/graphql/resolvers/Mutation/createGame.js
@@ -1,19 +1,17 @@
 import { Game, Player } from '#root/db/models';
 // import Player from '../Player';
 
-const createGameResolver = (_, { name, password, teams }, context) => {
+const createGameResolver = async (_, { name, password, teams }, context) => {
   const { userId } = context.res.locals.userSession.dataValues;
   if (!userId) throw new Error('not logged in or invalid user session');
 
-  const game = Game.create({ name, password, teams });
+  const game = await Game.create({ name, password, teams });
   const player = Player.create({
     userId,
     gameId: game.id,
     password: '',
     admin: true,
   });
-
-  console.log(player);
 
   return game;
 };
