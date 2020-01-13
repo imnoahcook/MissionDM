3bfa8d27eede207727416f50c6c581d138bbc831
Cooklord <imnoahcook@gmail.com>
2020-01-13 18:11:04 -0500

    make killtarget return false if user doesn't have a target instead of throwing an error

--- server/graphql/resolvers/Mutation/killTarget.js
+++ server/graphql/resolvers/Mutation/killTarget.js
@@ -2,13 +2,13 @@ import { Player } from '#root/db/models';
 
 const killTarget = async (_, { gameId, password }, context) => {
   const { userId } = context.res.locals.userSession.dataValues;
-  if (!userId) throw new Error('not logged in or invalid user session');
+  if (!userId) return false;
 
   const user = await Player.findOne({
     where: { userId: userId, gameId: gameId },
   });
 
-  if (!user.targetId) throw new Error('User does not have a target');
+  if (!user.targetId) return false;
 
   const target = await Player.findByPk(user.targetId);
 
