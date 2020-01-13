e455c88b4427b4f74a91b76a4cd76fce31cf3852
Cooklord <imnoahcook@gmail.com>
2020-01-13 14:20:57 -0500

    WIP changes to front end gameinfo.js

--- server/graphql/resolvers/GameInfo.js
+++ server/graphql/resolvers/GameInfo.js
@@ -1,22 +1,24 @@
 import { User, Player } from '#root/db/models';
 
 const GameInfo = {
-  target: async (_, { gameId }, context) => {
+  target: x => {
+    console.log(x);
     const { userId } = context.res.locals.userSession.dataValues;
     if (!userId) return;
-  
+    console.log('hello');
+
     const { targetId } = await Player.findOne({
       where: { userId: userId, gameId: gameId },
     });
-  
+
     if (!targetId) throw new Error('Invalid target Id');
-  
+
     const targetPlayer = await Player.findOne({
       where: { id: targetId },
     });
-  
+
     if (!targetPlayer) throw new Error('Invalid Player Id');
-  
+
     return User.findByPk(targetPlayer.userId);
   },
 };
