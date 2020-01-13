f0063cccf5bf148352c550003e6ea104ef642417
ctcook97 <ctcook97@gmail.com>
2020-01-13 14:14:54 -0500

    Add gameinfo resolver to index

--- server/graphql/resolvers/GameInfo.js
+++ server/graphql/resolvers/GameInfo.js
@@ -4,19 +4,18 @@ const GameInfo = {
   target: async (_, { gameId }, context) => {
     const { userId } = context.res.locals.userSession.dataValues;
     if (!userId) return;
-  
+
     const { targetId } = await Player.findOne({
       where: { userId: userId, gameId: gameId },
     });
-  
+
     if (!targetId) throw new Error('Invalid target Id');
-  
+
     const targetPlayer = await Player.findOne({
       where: { id: targetId },
     });
-  
     if (!targetPlayer) throw new Error('Invalid Player Id');
-  
+
     return User.findByPk(targetPlayer.userId);
   },
 };
--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -2,9 +2,11 @@ import * as Query from './Query';
 import * as Mutation from './Mutation';
 import UserSession from './UserSession';
 import Player from './Player';
+import GameInfo from './GameInfo';
 const resolvers = {
   Player,
   UserSession,
+  GameInfo,
   Query,
   Mutation,
 };
