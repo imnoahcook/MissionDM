a910c8e88a161f3c2e378d42bffc968aaa5fe0c2
Parker Lunn <parkerlunn@ufl.edu>
2020-01-13 23:39:56 -0500

    changed game buttons

--- client/src/components/Root/Pages/AccountDetails/Games/GamesList/GamesList.js
+++ client/src/components/Root/Pages/AccountDetails/Games/GamesList/GamesList.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import GameButton from './GameButton';
+import { ButtonList } from 'react-bulma-components';
 
 const GamesList = () => {
   const games = useSelector(state => state.game);
 
   if (!games) return 'Loading...';
 
-  return games.map(game => <GameButton key={game.id} {...game} />);
+  return (
+    <ButtonList position="centered">
+      {games.map(game => <GameButton key={game.id} {...game} />)}
+    </ButtonList>
+  );
 };
 
 export default GamesList;
