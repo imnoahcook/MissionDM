31eb4561d30bf3992f25c4272dfad2ee711e8fc9
Cooklord <imnoahcook@gmail.com>
2020-01-15 00:41:58 -0500

    remove some unused imports

--- client/src/components/Root/Pages/AccountDetails/Games/Games.jsx
+++ client/src/components/Root/Pages/AccountDetails/Games/Games.jsx
@@ -2,7 +2,6 @@ import React from 'React';
 import GamesList from './GamesList';
 import JoinGame from './JoinGame';
 
-import { Content } from 'react-bulma-components';
 const Games = () => {
   return (
     <>
