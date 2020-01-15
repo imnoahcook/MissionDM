71b8cbf9f5e21c54cd6a2645cfaa5ab75c61b8f4
Cooklord <imnoahcook@gmail.com>
2020-01-15 00:57:56 -0500

    make babel build scripts

---
+++ server/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _config = _interopRequireDefault(require("dotenv/config"));
+
+require("./webserver");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+//# sourceMappingURL=index.js.map
